eb74ec1f71f89218bcb5a6fd6a2ebe4cb46a1a9f
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-10-08 16:39:12 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,120 @@
+
+/*
+    The var keyword was replaced with the let keyword due to scoping issues. 
+*/
+function var_vs_let() {
+    for(let i = 0; i < 10; i++){
+        //Iterates i ten times
+    }
+    try{
+        //Throws a ReferenceError: i is not defined
+        console.log(`let i = ${i}`);
+    } catch(error) {
+        console.log(error);
+    }
+
+    for(var j = 0; j < 10; j++){
+        //Iterates j ten times
+    }
+
+    console.log(`var j = ${j}`);
+}
+
+/*
+Primitive/Value Types:
+    String
+    Number
+    Boolean
+    Symbol
+    undefined
+    null //In javascript it is used to clear the value of a variable
+Reference Types:
+    Object
+    Array
+    Function
+*/
+function usingTypeof() {
+    let value;
+    
+    value = "Kalle";
+    console.log(`${value} is a ${typeof value}`);
+
+    value = 1;
+    console.log(`${value} is a ${typeof value}`);
+
+    value = 1.5;
+    console.log(`${value} is a ${typeof value}`);
+
+    value = {};
+    console.log(`${value} is a ${typeof value}`);
+
+    value = [1,2,3];
+    console.log(`${value} is a ${typeof value}`);
+
+    value = null;
+    console.log(`${value} is a ${typeof value}`);
+
+    //undefined is actually a type and a value.
+    value = undefined;
+    console.log(`${value} is a ${typeof value}`);
+}
+
+function objectAccessors() {
+    const person = {name: "Kalle", age: 80};
+    console.log(`Accessing with . notation: ${person.name} ${person.age}`);
+    console.log(`Accessing with bracket notation: ${person["name"]} ${person["age"]}`);
+    //Example of usage:  If you dont know the name of the target property e.g if you want to get the property by the property.
+    let propertyName = "age";
+    console.log(`Accessing with bracket and variable name: ${person[propertyName]}`)
+}
+
+/*
+An array is an object.
+Used to represent a list of items.
+Arrays have dynamic length in Javascript.
+Arrays can store different types in the same array.
+*/
+function usingArrays() {
+    let listOfItems = [1, 1.3, 2];
+    console.log(listOfItems);
+    listOfItems[3] = 3.3;
+    console.log(listOfItems);
+    listOfItems[0] = "some text";
+    console.log(listOfItems);
+
+    console.log('\n');
+
+    //Iterate an array
+    for(let i = 0; i < listOfItems.length; i++) {
+        console.log(`The value in index ${i} is ${listOfItems[i]} and its type is ${typeof listOfItems[i]}`);
+    }
+
+    console.log('\n');
+
+    //Another way of iterating
+    listOfItems.forEach(element => console.log(element));
+}
+
+/*
+Functions can be called with fewer or more arguments then there are parameters.
+*/
+function functionExample(value) {
+    console.log(value);   
+}
+
+
+
+
+//var_vs_let();
+// usingTypeof();
+//objectAccessors();
+// usingArrays();
+
+// functionExample();
+// functionExample("Hello world");
+// functionExample("One", "Two");
+
+
+// let value = functionExample(10);
+// console.log(typeof value);
+
---
+++ oop.js
@@ -0,0 +1,113 @@
+/*
+The four pillars of OOP:
+    Encapsulation
+    Abstraction
+    Inheritance
+    Polymorphism
+*/
+
+// Encapsulation example with an object literal
+let employee = {
+    baseSalary: 30_000,
+    overtime: 10,
+    rate: 20,
+    getWage: function() {
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+
+// console.log(employee.getWage());
+
+// Factory example
+function createEmployee(baseSalary, overtime, rate){
+    return {
+        baseSalary,
+        overtime,
+        rate,
+        getWage: function() {
+            return this.baseSalary + (this.overtime * this.rate);
+        }
+    }
+}
+
+// let emp = createEmployee(10_000, 30, 20);
+// console.log(emp.getWage());
+// let emp2 = createEmployee(20_000, 30, 30);
+// console.log(emp2.getWage());
+
+/* 
+    Constructor function
+    Using the keyword new makes the keyword this reference to the function object
+*/
+function Employee(baseSalary, overtime, rate) {
+    // console.log('this', this);
+    this.baseSalary = baseSalary;
+    this.overtime = overtime;
+    this.rate = rate;
+
+    this.getWage = () => this.baseSalary + (this.overtime * this.rate);
+}
+
+/*
+let emp = new Employee(10_000, 30, 20); //Using the new keyword makes the Constructor function return the this keyword that references to the created object.
+console.log(emp.getWage());
+let emp2 = new Employee(20_000, 30, 30);
+console.log(emp2.getWage());
+Employee(1, 2, 3); //When called without the new keyword the this keyword inside the function will reference to the global scope.
+*/
+
+// Iterating properties in an object
+// let emp = new Employee(10_000, 30, 20);
+
+// for(let key in emp) {
+//     console.log(key, emp[key]);
+// }
+
+/*
+    Data Hiding
+    Closure: What members will be accessible to an inner function. Closure will continue in memory after the inner function ends.
+    Scope: Exists only until until the function ends.
+*/
+function Circle(radius) {
+
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 }; //Private member
+
+    //Property declaration get; set;
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            
+            if(!value.x || !value.y) {
+                throw new Error('Invalid location');
+            }
+
+            defaultLocation = value;
+        }
+    })
+
+    //Private member
+    let computeOptimumLocation = function(factor) {
+        // ...
+    }
+
+    this.draw = function() {
+        let x, y;
+
+        computeOptimumLocation(0.1);
+
+        console.log("draw");
+    }
+
+}
+
+// let c = new Circle(10);
+// c.draw();
+// console.log(c.defaultLocation);
+// c.defaultLocation = {x: 5, y: 10};
+// console.log(c.defaultLocation);
+
+
