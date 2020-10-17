a4a01d47a93a8400490ba202271e591b0887062d
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-10-17 13:21:38 +0200

    Closure example.

--- advanced.js
+++ advanced.js
@@ -24,3 +24,21 @@ const ages = [1969, 1975, 1991, 2005];
 
 const agesResult = calculateArray(ages, calculateAge);
 const overEighteenResult = calculateArray(ages, isOver18);
+
+/*
+Closure
+*/
+
+function interviewQuestions(job) {
+  //Closure makes the inner function keep the value of the "job" parameter even after
+  //the outer function has been terminated.
+  return function (name) {
+    if (job === "designer") {
+      console.log(name + " can you please explaing what UX-design is?");
+    } else {
+      console.log("Hello " + name + ", what do you do?");
+    }
+  };
+}
+
+interviewQuestions("homeless")("John");
