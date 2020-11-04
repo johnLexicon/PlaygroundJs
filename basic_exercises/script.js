718e9585e8cd5cc911ecb89a009084edf8cb2b6b
john lundgren <john.lundgren.lexicon@gmail.com>
2020-11-04 15:46:56 +0100

    Added solutions for basic exercises.

---
+++ basic_exercises/script.js
@@ -0,0 +1,24 @@
+
+// Number 6
+
+function e6(arr) {
+    for(let v of arr) {
+        alert(v);
+    }
+}
+
+// Number 7
+
+function e7() {
+    let again = false;
+    do {
+        again = confirm("Do you want to see me again?");
+    } while(again);
+}
+
+// Number 8
+
+function e8(fullName) {
+    const names = fullName.split(" ");
+    return `${names[1]} ${names[0]}`;
+}
