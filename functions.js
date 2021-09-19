56458c6c1e00271b28bab8fe4dcc9f3c559315e9
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-09-19 17:59:45 +0200

    Rest params and destructuring arguments.

--- functions.js
+++ functions.js
@@ -23,6 +23,35 @@ function getProps_2(obj, arr = []) {
 props = getProps_2({ id: '3', name: 'kalle', surname: 'anka' });
 console.log(props);
 
+/*-------------------- rest parameters example ---------------------*/
+
+function maxVal(first = -Infinity, ...rest) {
+  let max = first;
+  for (val of rest) {
+    if (val > max) {
+      max = val;
+    }
+  }
+  return max;
+}
+
+let max = maxVal(100, 10, 8, 7, 50);
+console.log(max);
+
+/*---------------- Destructuring arguments into parameters ------------*/
+
+function vectorAdd([x1, y1], [x2, y2]) {
+  return [x1 + x2, y1 + y2];
+}
+
+console.log([2, 3], [4, 5]);
+
+function vectorMultiply({ x, y }, scalar) {
+  return { x: x * scalar, y: y * scalar }; // Properties in argument must have same name as the ones in destructor
+}
+
+console.log(vectorMultiply({ x: 1, y: 2 }, 2));
+
 /*--------------------- this keyword examples ------------*/
 
 function whatisThis() {
