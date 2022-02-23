c04f4de564e07b8046ba722510d88ad9961f7eed
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2022-02-23 14:27:10 +0100

    Comparisons of Number object  with primitive number.

--- numberObjects.js
+++ numberObjects.js
@@ -1,5 +1,7 @@
-let x = new Number();
+let x = new Number(30);
 
 let y = 30;
 
-console.log(x == y);
+console.log(x == y); // True (Implicit coercion before comparison)
+console.log(x === y); // False
+console.log(Object.is(x, y));
